e84eae1fbe05f440dc3daa5efe97660173a35010
Luksona2006 <luka.bakuridze@redberry.ge>
2023-06-17 22:34:04 +0400

    feat: loading embodiment

--- src/pages/MovieListPage.vue
+++ src/pages/MovieListPage.vue
@@ -3,7 +3,7 @@
   <the-container class="grid sm:grid-cols-5 sm:mt-8 mt-4 pb-32 sm:px-[70px] px-9 items-start">
     <side-bar-component class="sm:grid hidden" />
     <div class="grid sm:col-span-4">
-      <div class="w-full flex justify-between items-start">
+      <div class="w-full flex justify-between items-start" v-if="totalMovies !== null">
         <p class="sm:block hidden text-2xl font-medium text-white">
           {{ $t('basic.my_list_of_movies') }} ({{ $t('basic.total') }} {{ totalMovies }})
         </p>
@@ -18,17 +18,24 @@
           <add-movie @add-new-movie="addNewMovie" />
         </div>
       </div>
-      <div class="grid sm:grid-cols-3 grid-cols-1 gap-12 mt-12">
+      <div class="grid sm:grid-cols-3 grid-cols-1 gap-12 mt-12" v-if="totalMovies !== null">
         <movie-component
           v-for="movie in movies"
           :movie="movie"
           :key="movie.id"
           v-if="movies.length > 0"
         />
+        <p
+          class="grid col-span-3 text-center text-3xl text-white font-bold py-16"
+          v-else-if="searchingValue === ''"
+        >
+          {{ $t('post.no_movies_yet') }}
+        </p>
         <p class="grid col-span-3 text-center text-3xl text-white font-bold py-16" v-else>
-          No movies yet
+          {{ $t('post.no_movies') }}
         </p>
       </div>
+      <loading-icon class="w-10 h-10 mt-10 mx-auto" :show="showLoading" />
     </div>
   </the-container>
 </template>
@@ -45,10 +52,12 @@ import TheContainer from '@/components/TheContainer.vue'
 import MovieComponent from '@/components/MovieComponent.vue'
 import SearchComponent from '@/components/SearchComponent.vue'
 import AddMovie from '@/components/popups/crud/AddMovie.vue'
+import LoadingIcon from '@/assets/icons/LoadingIcon.vue'
 
 const user = useUserStore()
 const movies = ref([])
-const totalMovies = ref(0)
+const totalMovies = ref(null)
+const showLoading = ref(true)
 
 const fetchStore = useFetchStore()
 
@@ -71,6 +80,8 @@ window.scrollTo({
 
 axiosInstance.get(`/user/${user.token}/movies/page/${fetchStore.page}`).then((res) => {
   if (res.status === 200) {
+    showLoading.value = false
+
     totalMovies.value = res.data.total
     movies.value.push(...res.data.movies)
 
@@ -84,11 +95,13 @@ axiosInstance.get(`/user/${user.token}/movies/page/${fetchStore.page}`).then((re
 })
 
 const searchingValue = ref('')
+const searchingValueChanged = ref(false)
 
 watch(
   () => searchingValue.value,
   () => {
     fetchStore.clearFetchStore()
+    searchingValueChanged.value = true
     movies.value = []
   }
 )
@@ -101,6 +114,8 @@ function searchData(searchBy) {
       axiosInstance
         .post('my-movies/search', { searchBy, user_token: user.token, pageNum: fetchStore.page })
         .then((res) => {
+          searchingValueChanged.value = false
+          showLoading.value = false
           movies.value.push(...res.data.movies)
 
           fetchStore.finishFetch()
@@ -117,6 +132,8 @@ function searchData(searchBy) {
       fetchStore.startFetch()
       axiosInstance.get(`/user/${user.token}/movies/page/${fetchStore.page}`).then((res) => {
         if (res.status === 200) {
+          searchingValueChanged.value = false
+          showLoading.value = false
           movies.value.push(...res.data.movies)
 
           fetchStore.finishFetch()
@@ -135,8 +152,13 @@ function searchData(searchBy) {
 function fetchDataOnScroll() {
   const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight
 
-  if (scrolledToBottom && !fetchStore.isFetching) {
-    searchData(searchingValue.value)
+  if (scrolledToBottom && !fetchStore.isFetching && !fetchStore.allPagesFetched) {
+    showLoading.value = true
+    fetchStore.startFetch()
+    const ms = Math.random() * (3500 - 2000) + 2000
+    setTimeout(() => {
+      searchData(searchingValue.value)
+    }, ms)
   }
 }
 
--- src/pages/NewsFeedPage.vue
+++ src/pages/NewsFeedPage.vue
@@ -3,7 +3,7 @@
   <the-container class="grid sm:grid-cols-4 sm:mt-8 mt-0 pb-32">
     <side-bar-component class="sm:grid hidden" />
     <div class="grid sm:col-span-2">
-      <div class="w-full flex items-start gap-2 sm:mb-5">
+      <div class="w-full flex items-start gap-2 sm:mb-5" v-if="quotesFetched">
         <write-new-quote
           class="transition-all duration-500 min-w-[219px]"
           :class="{ 'w-[286px]': searchOpened }"
@@ -11,7 +11,7 @@
         />
         <search-component @open-search="openSearch" @search-data="searchData" />
       </div>
-      <div class="flex flex-col sm:gap-20 gap-8">
+      <div class="flex flex-col sm:gap-20 gap-8" v-if="quotesFetched">
         <post-component
           v-for="quote in quotes"
           :quote="quote"
@@ -20,9 +20,11 @@
         />
         <p
           class="text-2xl font-bold text-white opacity-80 text-center mt-10"
-          v-else-if="searchingValue.startsWith('#') || searchingValue === ''"
+          v-else-if="
+            (searchingValue.startsWith('#') || searchingValue === '') && !searchingValueChanged
+          "
         >
-          No quotes found
+          {{ $t('post.no_quotes') }}
         </p>
 
         <movie-component
@@ -34,11 +36,12 @@
         />
         <p
           class="text-2xl font-bold text-white opacity-80 text-center mt-10"
-          v-else-if="searchingValue.startsWith('@')"
+          v-else-if="searchingValue.startsWith('@') && !searchingValueChanged"
         >
-          No movies found
+          {{ $t('post.no_movies') }}
         </p>
       </div>
+      <loading-icon class="w-10 h-10 mt-10 mx-auto" :show="showLoading" />
     </div>
   </the-container>
 </template>
@@ -56,8 +59,10 @@ import PostComponent from '@/components/PostComponent.vue'
 import MovieComponent from '@/components/MovieComponent.vue'
 import SearchComponent from '@/components/SearchComponent.vue'
 import WriteNewQuote from '@/components/popups/crud/WriteNewQuote.vue'
+import LoadingIcon from '@/assets/icons/LoadingIcon.vue'
 
 const searchOpened = ref(false)
+const showLoading = ref(true)
 
 function openSearch() {
   searchOpened.value = true
@@ -67,6 +72,7 @@ const user = useUserStore()
 const quotes = ref([])
 const movies = ref([])
 const fetchStore = useFetchStore()
+const quotesFetched = ref(false)
 
 onMounted(() => {
   fetchStore.clearFetchStore()
@@ -86,7 +92,10 @@ window.scrollTo({
 })
 
 axiosInstance.get(`/user/${user.token}/quotes/page/${fetchStore.page}`).then((res) => {
+  showLoading.value = true
   if (res.status === 200) {
+    quotesFetched.value = true
+    showLoading.value = false
     quotes.value.push(...res.data.quotes)
 
     if (res.data.isLastPage === true) {
@@ -99,11 +108,14 @@ axiosInstance.get(`/user/${user.token}/quotes/page/${fetchStore.page}`).then((re
 })
 
 const searchingValue = ref('')
+const searchingValueChanged = ref(false)
 
 watch(
   () => searchingValue.value,
   () => {
     fetchStore.clearFetchStore()
+
+    searchingValueChanged.value = true
     quotes.value = []
     movies.value = []
   }
@@ -113,45 +125,52 @@ function searchData(searchBy) {
   searchingValue.value = searchBy
   if (fetchStore.allPagesFetched === false) {
     if (searchBy.startsWith('#')) {
-      fetchStore.startFetch()
       axiosInstance
         .post('quotes/search', { searchBy, user_token: user.token, pageNum: fetchStore.page })
         .then((res) => {
-          quotes.value.push(...res.data.quotes)
-          searchOpened.value = false
+          if (res.status === 200) {
+            searchingValueChanged.value = false
+            showLoading.value = false
+            quotes.value.push(...res.data.quotes)
+            searchOpened.value = false
 
-          fetchStore.finishFetch()
-          if (res.data.isLastPage === true) {
-            fetchStore.allDataFetched()
-            return true
+            fetchStore.finishFetch()
+            if (res.data.isLastPage === true) {
+              fetchStore.allDataFetched()
+              return true
+            }
+
+            fetchStore.increasePageNum()
           }
-
-          fetchStore.increasePageNum()
         })
     }
 
     if (searchBy.startsWith('@')) {
-      fetchStore.startFetch()
       axiosInstance
         .post('movies/search', { searchBy, user_token: user.token, pageNum: fetchStore.page })
         .then((res) => {
-          movies.value.push(...res.data.movies)
-          searchOpened.value = false
+          if (res.status === 200) {
+            searchingValueChanged.value = false
+            showLoading.value = false
+            movies.value.push(...res.data.movies)
+            searchOpened.value = false
 
-          fetchStore.finishFetch()
-          if (res.data.isLastPage === true) {
-            fetchStore.allDataFetched()
-            return true
+            fetchStore.finishFetch()
+            if (res.data.isLastPage === true) {
+              fetchStore.allDataFetched()
+              return true
+            }
+
+            fetchStore.increasePageNum()
           }
-
-          fetchStore.increasePageNum()
         })
     }
 
     if (searchBy === '') {
-      fetchStore.startFetch()
       axiosInstance.get(`/user/${user.token}/quotes/page/${fetchStore.page}`).then((res) => {
         if (res.status === 200) {
+          searchingValueChanged.value = false
+          showLoading.value = false
           quotes.value.push(...res.data.quotes)
           searchOpened.value = false
 
@@ -170,9 +189,14 @@ function searchData(searchBy) {
 
 function fetchDataOnScroll() {
   const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight
-
-  if (scrolledToBottom && !fetchStore.isFetching) {
-    searchData(searchingValue.value)
+  console.log(scrolledToBottom && !fetchStore.isFetching && !fetchStore.allPagesFetched)
+  if (scrolledToBottom && !fetchStore.isFetching && !fetchStore.allPagesFetched) {
+    showLoading.value = true
+    fetchStore.startFetch()
+    const ms = Math.random() * (3500 - 2000) + 2000
+    setTimeout(() => {
+      searchData(searchingValue.value)
+    }, ms)
   }
 }
 
