af0065f37de24d14e20c18e5bf7b59fcada959e9
Luksona2006 <luka.bakuridze@redberry.ge>
2023-06-19 22:36:47 +0400

    chore: paste url from env file

--- src/config/axios/getCookies.js
+++ src/config/axios/getCookies.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 function getCookies() {
   axios
-    .get('http://localhost:8000/sanctum/csrf-cookie', {
+    .get(`${import.meta.env.VITE_API_URL}/sanctum/csrf-cookie`, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
